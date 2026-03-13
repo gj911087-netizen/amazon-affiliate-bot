763a732d60ace8b0f8a85c8ec28e2a37b8b05634
gj911087-netizen <gj911087@gmail.com>
2026-03-12 19:52:28 -0500

    Implement find_products function to limit results

---
+++ product_engine.py
@@ -0,0 +1,11 @@
+rom config import PRODUCT_LIMIT
+
+def find_products():
+
+    products = [
+        "B0ABC12345",
+        "B0XYZ67890",
+        "B0TEST11111"
+    ]
+
+    return products[:PRODUCT_LIMIT]
