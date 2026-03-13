410dce2c029542aaaf01842f06d7fa21eaf44a35
gj911087-netizen <gj911087@gmail.com>
2026-03-12 19:48:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+from product_engine import find_products
 from affiliate_links import generate_affiliate_link
+from marketing_ai import generate_marketing_text
+from image_generator import generate_image
+from autoposter import post_to_social
+from analytics import log_post
 
-asin = "B0ABC12345"
 
-link = generate_affiliate_link(asin)
+products = find_products()
 
-print("Affiliate link:", link)
+for asin in products:
+
+    link = generate_affiliate_link(asin)
+
+    text = generate_marketing_text("Producto Amazon", link)
+
+    image = generate_image("Producto Amazon")
+
+    post_to_social(text, image)
+
+    log_post("Producto Amazon", link)
